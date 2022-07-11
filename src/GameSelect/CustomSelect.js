48f4c2a59caae4496986cb139ce61ad626193244
Hakob <45873619+HakobHakob@users.noreply.github.com>
2022-07-11 22:52:23 +0400

    Add files via upload

---
+++ src/GameSelect/CustomSelect.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { SelectStyle } from './SelectStyle'
+
+const CustomSelect = ({ options, onChange }) => {
+  return (
+    <SelectStyle onChange={onChange}>
+      {options.map((option, i) => (
+        <option key={i} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </SelectStyle>
+   
+  )
+}
+
+export { CustomSelect }
